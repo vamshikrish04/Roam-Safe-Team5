599c541bfdcd7441b1d0311f916cc57e9f02b1b5
vamshi <vkrishna.garrepally04@gmail.com>
2023-09-17 16:30:00 +0530

    adding dashboard page

---
+++ roam-safe-admin/src/Pages/dashboard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import AdminLayout from "../Layout";
+
+export default function Dashboard() {
+  return (
+    <AdminLayout>
+      <h1>Dashboard</h1>
+    </AdminLayout>
+  );
+}
