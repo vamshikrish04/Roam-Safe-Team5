dee1a9266bb5051234cae073dbbf635b541fe541
vamshi <vkrishna.garrepally04@gmail.com>
2023-11-02 19:35:00 +0530

    re-added delete property

--- roam-safe-admin/src/Pages/contact.js
+++ roam-safe-admin/src/Pages/contact.js
@@ -65,6 +65,21 @@ export default function Contact() {
     }
   };
   
+  const deleteProperty = (index) => {
+    swal({
+      title: "Are you sure?",
+      text: "Once deleted, you will not be able to recover this imaginary file!",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        deleteData(index);
+      } else {
+        swal("Your imaginary file is safe!");
+      }
+    });
+  };
 
   const deleteData = async (index) => {
     const { _id } = data[index];
