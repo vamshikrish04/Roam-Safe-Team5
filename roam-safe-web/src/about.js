ba5f3ea7451fc4d200db97cafb0e0c5114e842c6
susheelambati <susheelambati24@gmail.com>
2023-11-03 23:15:00 +0530

    working changes in about

---
+++ roam-safe-web/src/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Banner from "../Components/About/banner";
+import SecondSection from "../Components/About/second-section";
+import Layout from "../Layout/layout";
+
+export default function About() {
+  return (
+    <Layout>
+     <Banner/>
+     <SecondSection/>
+    </Layout>
+  );
+}
