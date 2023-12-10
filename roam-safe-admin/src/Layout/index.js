a4f6caa984f4fab467f1041a6442da9121c48fb7
susheelambati <susheelambati24@gmail.com>
2023-12-10 03:03:09 -0500

    added working changes

--- roam-safe-admin/src/Layout/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import LayoutTemplate from "./LayoutPage/sidenav";
-
-const AdminLayout = ({ children }) => {
-  return <LayoutTemplate>{children}</LayoutTemplate>;
-};
-
-export default AdminLayout;
